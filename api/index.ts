54812d27238f5f0bc74283627a13416e63ae63ae
Hung Le <hung.le@optimizely.com>
2023-10-10 21:49:44 +0700

    update api

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import webPush from 'web-push';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-// that key has been add to vercel like environment variable
 const vapidKeys = {
   publicKey: `${process.env.VITE_PUBLIC_KEY}`,
   privateKey: `${process.env.VITE_PRIVATE_KEY}`,
@@ -17,6 +19,14 @@ webPush.setVapidDetails(
 
 app.get('/api', (req, res) => {
   res.end(`Hello! This is a control page`);
+});
+
+app.get('/api/get-vapid-keys', async (req, res) => {
+  return res.status(200).json(
+    {
+      vapidKeys
+    }
+  );
 });
 
 app.get('/api/trigger-push-message', async (req, res) => {
