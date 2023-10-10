b6ed29e937c55034b6851cba156da6098486aa7b
Hung Le <hung.le@optimizely.com>
2023-10-10 22:47:35 +0700

    update api

--- api/index.ts
+++ api/index.ts
@@ -24,7 +24,7 @@ app.get('/api', (req, res) => {
   res.end(`Hello! This is a control page`);
 });
 
-app.get('/api/get-vapid-keys', async (req, res) => {
+app.get('/api/get-vapid-keys', (req, res) => {
   return res.status(200).json(
     {
       vapidKeys
@@ -32,18 +32,10 @@ app.get('/api/get-vapid-keys', async (req, res) => {
   );
 });
 
-app.post('/api/trigger-push-message', async (req, res) => {
-  if(req.method.toLowerCase() !== 'post') {
-    res.status(405).json({
-      error: "Only support POST method"
-    });
-
-    return;
-  }
-
+app.post('/api/trigger-push-message', (req, res) => {
   return res.status(200).json(
     {
-      dataSent: JSON.stringify(req.body),
+      dataSent: req.body,
       test: 'test',
       vapidKeys
     }
---
+++ server.ts
@@ -0,0 +1,5 @@
+import app from "./api/index.ts";
+
+app.listen(8999,() => {
+  console.log('http://localhost:8999/');
+});
