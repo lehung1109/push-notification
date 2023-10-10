1e5ca593d1ace76dae9b113fa929feed31882aa1
Hung Le <hung.le@optimizely.com>
2023-10-10 21:39:48 +0700

    update api

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+
+const app = express();
+
+// that key has been add to vercel like environment variable
+const vapidKeys = {
+  publicKey: `${process.env.VITE_PUBLIC_KEY}`,
+  privateKey: `${process.env.VITE_PRIVATE_KEY}`,
+};
+
+app.get('/api', (req, res) => {
+  res.end(`Hello! This is a control page`);
+});
+
+app.get('/api/trigger-push-message', async (req, res) => {
+  return res.status(200).json(
+    {
+      dataSent: req.body,
+      vapidKeys
+    }
+  );
+});
+
+module.exports = app;
